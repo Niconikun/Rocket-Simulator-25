67b6ee02193023516b8a50a2dc1b8822e20dac22
Niconikun <nicolassepulvedaestrada@gmail.com>
2025-06-29 09:47:55 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 rocket_page = st.Page("Simulation Settings/create.py", title="Rocket", icon=":material/add_circle:")
 sim_page = st.Page("Simulation Settings/sim_settings.py", title="Simulation", icon=":material/settings:")
-dashboard_page = st.Page("delete.py", title="Dashboard", icon=":material/delete:")
+dashboard_page = st.Page("Analysis/delete.py", title="Dashboard", icon=":material/delete:")
 
 pg = st.navigation([rocket_page, sim_page, dashboard_page])
 st.set_page_config(page_title="Data manager", page_icon=":material/edit:")
