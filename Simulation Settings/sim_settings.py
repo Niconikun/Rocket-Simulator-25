b61010497777666c4815bd7f0b1a3e44f90622c1
Niconikun <nicolassepulvedaestrada@gmail.com>
2025-06-29 20:26:38 -0400

    added other inputs

--- Simulation Settings/sim_settings.py
+++ Simulation Settings/sim_settings.py
@@ -1,17 +1,28 @@
 import streamlit as st
 
-left_column, right_column = st.columns(2)
-# You can use a column just like st.sidebar:
-left_column.button('Press me!')
-options = ["North", "East", "South", "West"]
-left_column.text_input('Enter your name:', 'Type here...')
-selection = left_column.pills("Directions", options, selection_mode="multi")
-st.markdown(f"Your selected options: {selection}.")
+with st.form("Simulation Settings"):
+    st.write("Rocket Settings")
 
+    left_column, right_column = st.columns(2)
 
-# Or even better, call Streamlit functions inside a "with" block:
-with right_column:
-    chosen = st.radio(
-        'Sorting hat',
-        ("Gryffindor", "Ravenclaw", "Hufflepuff", "Slytherin"))
-    st.write(f"You are in {chosen} house!")
+    with left_column:
+        #info
+        st.subheader("Simulation Properties")
+        sim_runtime = st.slider('Simulation runtime [s]', min_value=0.0, max_value=10000.0, value=500.0, step=1.0)
+        sim_time_step = st.number_input('Simulation time step [s]', min_value=0.0, max_value=10.0, value=0.1, step=0.01)
+        sim_date = st.date_input('Simulation date', value=None, min_value=None, max_value=None, key=None)
+        sim_time = st.time_input('Simulation time', value=None, key=None)
+        sim_timezone = st.selectbox('Simulation timezone', options=['UTC', 'Local'], index=0)   
+
+    with right_column:
+        #info
+        st.subheader("Simulation Settings")
+        sim_rocket = st.selectbox('Rocket Selection', options=['UTC', 'Local'], index=0)
+        sim_location = st.selectbox('Location Selection', options=['UTC', 'Local'], index=0)
+        average_temperature = st.number_input('Average temperature [C]', min_value=-50.0, max_value=50.0, value=20.0, step=1.0)
+        launch_elevation = st.number_input('Launch elevation [m]', min_value=0.0, max_value=10000.0, value=0.0, step=1.0)
+        launch_site_orientation = st.number_input('Launch site orientation', min_value=-50.0, max_value=50.0, value=20.0, step=1.0)
+        average_pressure = st.number_input('Average pressure [Pa]', min_value=0.0, max_value=100000.0, value=101325.0, step=1.0)
+        average_humidity = st.number_input('Average humidity [%]', min_value=0.0, max_value=100.0, value=50.0, step=1.0)
+    # Add a submit button
+    submitted = st.form_submit_button("Run Simulation")
--- streamlit_app.py
+++ streamlit_app.py
@@ -2,11 +2,12 @@ import streamlit as st
 
 rocket_page = st.Page("Simulation Settings/rocket_settings.py", title="Rocket", icon=":material/add_circle:")
 sim_page = st.Page("Simulation Settings/sim_settings.py", title="Simulation", icon=":material/settings:")
+location_page = st.Page("Simulation Settings/location_settings.py", title="Location", icon=":material/location_on:")
 dashboard_page = st.Page("Analysis/dashboard.py", title="Dashboard", icon=":material/delete:")
 
 pg = st.navigation(
         {
-            "Simulation Settings": [rocket_page, sim_page],
+            "Simulation Settings": [rocket_page, location_page, sim_page],
             "Analysis": [dashboard_page],
         }
     )
