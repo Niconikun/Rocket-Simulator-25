a00f43a0080052104ce87162ca12698fabbcf42c
Niconikun <nicolassepulvedaestrada@gmail.com>
2025-08-17 02:04:10 -0400

    ye

---
+++ src/models/engine/constants.py
@@ -0,0 +1,7 @@
+"""contains constants needed for motor calculations."""
+
+# R, in units of J/(kmol*K)
+gasConstant = 8314.462618
+
+# Standard gravitation acceleration (g), in units of m/s**2
+standardGravity = 9.80665
